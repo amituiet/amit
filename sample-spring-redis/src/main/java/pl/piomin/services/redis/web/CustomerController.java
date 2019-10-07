a32bd8e3b6ccb2a778c0718989e9728c5f821906
amituiet <41050392+amituiet@users.noreply.github.com>
2019-10-07 10:10:24 +0530

    Add files via upload
    Vivek

---
+++ sample-spring-redis/src/main/java/pl/piomin/services/redis/web/CustomerController.java
@@ -0,0 +1,32 @@
+package pl.piomin.services.redis.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.piomin.services.redis.model.Customer;
+import pl.piomin.services.redis.model.Transaction;
+import pl.piomin.services.redis.repository.CustomerRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @PostMapping
+    public Customer add(@RequestBody Customer customer) {
+        return repository.save(customer);
+    }
+
+    @GetMapping("/{id}")
+    public Customer findById(@PathVariable("id") Long id) {
+        Optional<Customer> optCustomer = repository.findById(id);
+        if (optCustomer.isPresent())
+            return optCustomer.get();
+        else
+            return null;
+    }
+
+}
